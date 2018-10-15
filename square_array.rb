9d9bb5fd8c7d54e7be4f4c62aa541e81d3ce40a5
bcarrol2 <blairacarroll@gmail.com>
2018-10-15 20:23:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 new_numbers = [9,10,16,25]
 
 def square_array(numbers)
-  numbers.each do |i| puts i **2
+  numbers.each do {|i| puts i **2}
   end
 end
 puts square_array(numbers)
