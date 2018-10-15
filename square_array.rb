f65263a395ba3d8a1c70e6425881fa6e56b91112
bcarrol2 <blairacarroll@gmail.com>
2018-10-15 20:38:32 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,6 @@
-numbers = [1,2,3]
-new_numbers = [9,10,16,25]
-
-def square_array(numbers)
-  numbers.each do |i| puts i **2
-  end
-end
-square_array(numbers)
-
-def square_array(new_numbers)
-  new_numbers.each do |i| puts i **2
-  end
-end
-square_array(new_numbers)
+def square_array(array)
+  # your code here
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
+end 
