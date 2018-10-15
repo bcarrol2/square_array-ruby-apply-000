36acb62c1925ba1b9f1f5ab3762c92a555f90e6f
bcarrol2 <blairacarroll@gmail.com>
2018-10-15 20:28:22 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,3 @@
-numbers = [1,2,3]
-new_numbers = [9,10,16,25]
-
-def square_array(numbers)
-  numbers.each do |i| puts i **2
+square_array = [1,2,3]
+  [1,2,3].each do |i| puts i **2
   end
-end
-puts square_array(numbers)
-
-square_array(new_numbers)
-  new_numbers.each do |i| puts i **2
-  end
-square_array(new_numbers)
